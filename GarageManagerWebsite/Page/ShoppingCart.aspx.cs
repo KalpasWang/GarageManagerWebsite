db6362b5030cfb4b8a6115b17fd7c43279a5c3bd
Kalpas Wang <wf6212@gmail.com>
2019-04-24 23:25:06 +0800

    shopping cart layout 2

--- GarageManagerWebsite/Page/ShoppingCart.aspx.cs
+++ GarageManagerWebsite/Page/ShoppingCart.aspx.cs
@@ -58,7 +58,55 @@ namespace GarageManagerWebsite.Page
                     ImageUrl = product.Image,
                     PostBackUrl = "~/Page/Details.aspx?id=" + product.Id
                 };
+
+                LinkButton linkDelete = new LinkButton
+                {
+                    ID = "del" + purchase.Id,
+                    Text = "Delete",
+                    ForeColor = System.Drawing.Color.Red
+                };
+                linkDelete.Click += linkDelete_DeleteItem;
+
+                var range = Enumerable.Range(1, 10);
+                DropDownList ddlAmount = new DropDownList
+                {
+                    ID = purchase.Id.ToString(),
+                    AutoPostBack = true,
+                    DataSource = range
+                };
+
+                ddlAmount.DataBind();
+                ddlAmount.SelectedValue = purchase.Amount.ToString();
+                ddlAmount.SelectedIndexChanged += ddlAmount_SelectedIndexChanged;
+
+                Table cartTable = new Table { CssClass = "cartTable" };
+                TableRow rowA = new TableRow();
+                TableRow rowB = new TableRow();
+
+                TableCell cellA1 = new TableCell { RowSpan = 2, Width = 50 };
+                TableCell cellA2 = new TableCell { Text = $"<h4>{product.Name}</h4><br/>{product.Id}<br/>In Stock" };
+                TableCell cellA3 = new TableCell { Text = "Unit Price:<hr>" };
+                TableCell cellA4 = new TableCell { Text = "Amount:<hr>" };
+                TableCell cellA5 = new TableCell { Text = "Total:<hr>" };
+                TableCell cellA6 = new TableCell { RowSpan = 2 };
+
+                TableCell cellB1 = new TableCell();
+                TableCell cellB2 = new TableCell { Text = string.Format("{0:c}", product.Price) };
+                TableCell cellB3 = new TableCell();
+                TableCell cellB4 = new TableCell { Text = string.Format("{0:c2}", product.Price*purchase.Amount) };
+                TableCell cellB5 = new TableCell();
+
             }
+        }
+
+        private void ddlAmount_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void linkDelete_DeleteItem(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
